a4d6e950a9d05f03a429a78b255a58254ecd74ee
sumit <sumit@localhost.localdomain>
2020-10-02 13:36:30 +0530

    fix security loop

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # custom-spf-checker
 For custom SPF (Sender Policy Framework) checker, This function check SPF record is valid or not 
+1 // if valid
+0 // if invalid
--- check_spf.php
+++ check_spf.php
@@ -13,18 +13,27 @@ function check_spf_valid_or_not($domain_name)
         }
     }
     $spf_ip_include_valid .= ($count > 1) ? '0' : '' ;
+    // echo $count;
+    
+
     foreach($result as $val)
-    { 
+    {
+        
+        
         //if it is spf record
         //get spf record string
         if(preg_match("#^v=spf1(.*)$#i", $val['txt']))
         {
+            // echo $val['txt'];
+            // echo "<br>";
             $spf_value = explode(" ", $domain_record->spf_record);
-
+            // var_dump($spf_value);
             //check valid spf record or not 
             //if match full spf
+            // $spf_ip_include_valid = '';
             if($val['txt'] == $domain_record->spf_record)
             {
+                // $valid=1;
                 $spf_ip_include_valid .= '1';
             }
             //check indivigual ip4 and include part
@@ -49,6 +58,8 @@ function check_spf_valid_or_not($domain_name)
                 {
                     $spf_ip_include_valid .= '0';
                 }
+
+
                 foreach($spf_value as $spf)
                 {
                     //check ip4 part
@@ -64,6 +75,10 @@ function check_spf_valid_or_not($domain_name)
                             $spf_ip_include_valid .= '0';
                         }
                     }
+                    else
+                        {
+                            $spf_ip_include_valid .= '0';
+                        }
 
                     //check ip6 part
                     if(@preg_match("#^ip6(.*)$#i", $spf))
@@ -78,6 +93,10 @@ function check_spf_valid_or_not($domain_name)
                             $spf_ip_include_valid .= '0';
                         }
                     }
+                    else
+                        {
+                            $spf_ip_include_valid .= '0';
+                        }
 
                     //check include part
                     if(@preg_match("#^include(.*)$#i", $spf))
@@ -92,22 +111,28 @@ function check_spf_valid_or_not($domain_name)
                             $spf_ip_include_valid .= '0';
                         }
                     }
+                    else
+                        {
+                            $spf_ip_include_valid .= '0';
+                        }
                 }
                 
 
             }
         }
     }
+    // echo $spf_ip_include_valid;die;
+    $valid = 0;
     if(strpos($spf_ip_include_valid, '0') || @preg_match("#^0(.*)$#i", $spf_ip_include_valid))
     {
         $valid = 0;
     }
-    else
+    elseif ( (!strpos($spf_ip_include_valid, '0') || !@preg_match("#^0(.*)$#i", $spf_ip_include_valid)) && (strpos($spf_ip_include_valid, '1') || @preg_match("#^1(.*)$#i", $spf_ip_include_valid) ) )
     {
         $valid=1;
     }
     return $valid;
 }
 
-//end of spf checker helper
+//end 
 
